330ffa4166f4742a75515f6b46e921f3447e5fd7
lucasroscundev <166140985+lucasroscundev@users.noreply.github.com>
2024-06-19 12:04:50 -0300

    Added Folders Prisma, SRC and Test

---
+++ src/infra/http/controllers/update-account.controller.ts
@@ -0,0 +1,79 @@
+import {
+    BadRequestException,
+    Body,
+    ConflictException,
+    Controller,
+    HttpCode,
+    Patch,
+    Post,
+    UsePipes,
+  } from '@nestjs/common'
+  import { z } from 'zod'
+  import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+  import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+  import { Public } from '@/infra/auth/public'
+  import { NestRegisterUserUseCase } from '@/infra/representations/nest-register-user-use-case'
+  import { UsersRepository } from '@/domain/forum/application/repositories/users-repository'
+  
+  const createAccountBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    nickname: z.string(),
+    picture: z.string(), 
+    given_name: z.string(), 
+    family_name: z.string(),      
+  })
+  
+  type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+  
+  @Controller('/accounts/update')
+  @Public()
+  export class CreateAccountController {
+    constructor(
+      private registerUser: NestRegisterUserUseCase,
+      private userRepository: UsersRepository,
+      /*private hashGenerator: HashGenerator,*/
+    ) {}
+  
+    @Patch()
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+    async handle(@Body() body: CreateAccountBodySchema) {
+      const { 
+        name, 
+        email, 
+        password, 
+        nickname,
+        picture,      
+        given_name, 
+        family_name,      
+        } = body
+  
+      const result = await this.registerUser.execute({
+        name,
+        email,
+        password,
+        nickname,
+        picture,
+        email_verified: true,       
+        given_name, 
+        family_name,
+        is_auth0_user: true,
+        createdAt: new Date(),       
+      })
+  
+      /*if (result.isLeft()) {
+        aw
+        const error = result.value
+  
+        switch (error.constructor) {
+          case StudentAlreadyExistsError:
+            throw new ConflictException(error.message)
+          default:
+            throw new BadRequestException(error.message)
+        }
+      }*/
+    }
+  }
+  
